30f148fc5994a44a96f4b4af72d181be8b6936f9
superfree <superfreeeee@gmail.com>
2024-08-03 10:28:31 +0800

    chore: use vitest instead of jest

--- src/libs/defer-promise/__tests__/defer.test.ts
+++ src/libs/defer-promise/__tests__/defer.test.ts
@@ -1,40 +1,50 @@
 import { defer } from '..';
 
 describe('defer-promise tests', () => {
-  test('resolve test', (done) => {
-    const deferred = defer<number>();
+  test('resolve test', () => {
+    return new Promise<void>((resolve) => {
+      const deferred = defer<number>();
 
-    deferred.promise.then((res) => {
-      expect(res).toBe(1);
-      done();
-    });
-
-    deferred.resolve(1);
-  });
-
-  test('reject test', (done) => {
-    const deferred = defer<number>();
-
-    const error = new Error('error test');
-
-    deferred.promise
-      .then((res) => {
-        fail('unexpected branch');
-      })
-      .catch((err) => {
-        expect(err).toBe(error);
-        done();
+      deferred.promise.then((res) => {
+        expect(res).toBe(1);
+        resolve();
       });
 
-    deferred.reject(error);
+      deferred.resolve(1);
+    });
   });
 
-  test('timeout test', (done) => {
-    const deferred = defer<number>({ timeout: 0 });
+  test('reject test', () => {
+    return new Promise<void>((resolve) => {
+      const deferred = defer<number>();
 
-    deferred.promise.catch((err) => {
-      expect(err).toBeInstanceOf(Error);
-      done();
+      const error = new Error('error test');
+
+      deferred.promise
+        .then(() => {
+          throw new Error('unexpected branch');
+        })
+        .catch((err) => {
+          expect(err).toBe(error);
+          resolve();
+        });
+
+      deferred.reject(error);
+    });
+  });
+
+  test('timeout test', () => {
+    return new Promise<void>((resolve) => {
+      const deferred = defer<number>({ timeout: 0 });
+
+      deferred.promise
+        .then(() => {
+          throw new Error('unexpected branch');
+        })
+        .catch((err) => {
+          expect(err).toBeInstanceOf(Error);
+          resolve();
+        });
     });
   });
 });
